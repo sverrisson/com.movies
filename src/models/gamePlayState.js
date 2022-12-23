c2a5af759175d786c845e99d68c10d6c89de0dcd
Hannes Sverrisson <hannessv@gmail.com>
2022-12-23 20:28:56 +0100

    Create GamePlayState and update Lint to allow private fields

---
+++ src/models/gamePlayState.js
@@ -0,0 +1,30 @@
+// Used for all the Game Logic to seperate that from the UI
+export default class GamePlayState {
+  // Private Fields
+  #score = 0
+  #selectedActorID
+  #correctMovieID
+
+  constructor() {
+    this.selectedActor = this.selectedActor.bind(this)
+    this.movieSelected = this.movieSelected.bind(this)
+  }
+
+  // When the user picks an actor
+  selectedActor(actorID) {
+    this.#selectedActorID = actorID
+
+    // Arrange movies to show based on the actorID
+  }
+
+  // When the user tries to pick the correct movie
+  movieSelected(movieID) {
+    if (movieID === this.#correctMovieID) {
+      // He picked correctly, increase the score
+      this.#score += 1
+    } else {
+      // Wrong answer, decrease the score, we allow negative score
+      this.#score -= 1
+    }
+  }
+}
