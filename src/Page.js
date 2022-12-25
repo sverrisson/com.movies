ae87f2be42336ff3ec162cf42c122d670821fc3b
Hannes Sverrisson <hannessv@gmail.com>
2022-12-25 23:35:47 +0100

    Background animation disabled

--- src/Page.js
+++ src/Page.js
@@ -49,12 +49,13 @@ export default class Page extends Lightning.Component {
     //       {
     //         t: '',
     //         p: 'color',
-    //         v: { 0: { v: 0xfffbb03b }, 0.5: { v: 0xfff46730 }, 0.8: { v: 0xffcbc0cb } },
+    //         v: { 0: { v: 0xfffbb03b }, 0.5: { v: 0xfff46730 }, 1: { v: 0xffcbc0cb } },
     //       },
     //     ],
     //   })
     //   .start()
 
+    // Update the Score on each page
     const score = this.fireAncestors('$getScore')
     if (score) {
       this.tag('Score').patch({
