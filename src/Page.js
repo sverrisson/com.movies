df64042a21633550462b45cf1f697b756d22d2be
Hannes Sverrisson <hannessv@gmail.com>
2022-12-25 23:41:56 +0100

    Header and Fadetop not used

--- src/Page.js
+++ src/Page.js
@@ -7,19 +7,6 @@ export default class Page extends Lightning.Component {
       w: 1920,
       h: 1080,
       Content: {},
-      FadeTop: {
-        w: 1920,
-        h: 230,
-        rect: true,
-        colorTop: Colors('white').alpha(0.7).get(),
-        colorBottom: 0x00000000,
-      },
-      Header: {
-        x: 90,
-        y: 100,
-        color: Colors('white').get(),
-        text: { text: this.header, fontSize: 50 },
-      },
       Background: {
         w: 1920,
         h: 1080,
