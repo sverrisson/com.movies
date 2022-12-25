28bc4919bd9be7457f4da3f68689e4e2430fef38
Hannes Sverrisson <hannessv@gmail.com>
2022-12-25 23:11:44 +0100

    App Updated with Game Logic

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
-import { Lightning, Utils } from '@lightningjs/sdk'
+import { Utils, Router, Log } from '@lightningjs/sdk'
+import GameState from './models/gameState'
+import Model from './models/model'
+import router from './routes'
 
-export default class App extends Lightning.Component {
+export default class App extends Router.App {
+  #gameState
+  #model
+
+  // GamePlayEvents
+  $startGame() {
+    Log.info('startGame')
+    Router.navigate('home')
+  }
+
+  $getScore() {
+    return this.#gameState.getScore()
+  }
+
+  $actorSelected(actor) {
+    Log.info('Actor Selected', actor)
+    // Pick one movie for the actor
+    const actorMovie = actor.movies.sort(() => Math.random() - 0.5)[0]
+    this.#gameState.setCorrectMovieID(actorMovie.id)
+    let movies = [actorMovie]
+    // Get the other random movies
+    this.#model.moviesWithoutActor(actor.actorID).then((otherMovies) => {
+      const shuffledMovies = movies.concat(otherMovies).sort(() => Math.random() - 0.5)
+      Router.navigate(`movies/${actor.actorID}`, { actor, movies: shuffledMovies })
+    })
+  }
+
+  $movieSelected(movieID) {
+    Log.info('Movie Selected', movieID)
+    this.#gameState.movieSelected(movieID)
+    Router.navigate('home')
+  }
+
   static getFonts() {
     return [{ family: 'Regular', url: Utils.asset('fonts/Roboto-Regular.ttf') }]
   }
 
   static _template() {
     return {
+      ...super._template(),
       Background: {
         w: window.innerWidth, // 1920
         color: 0xfffbb03b, // 1080
         src: Utils.asset('images/background.png'),
-        zIndex: -100,
+        zIndex: 100,
       },
     }
   }
 
-  _init() {
-    this.tag('Background')
-      .animation({
-        duration: 3,
-        repeat: -1,
-        actions: [
-          {
-            t: '',
-            p: 'color',
-            v: { 0: { v: 0xfffbb03b }, 0.5: { v: 0xfff487ff }, 0.8: { v: 0xfffbb03b } },
-          },
-        ],
-      })
-      .start()
+  _setup() {
+    // Setup routing
+    Router.startRouter(router, this)
+    Log.info('Initialise PlayState and Model')
+    this.#gameState = GameState.create()
+    this.#model = new Model()
+  }
+
+  _handleAppClose() {
+    Router.navigate('$')
   }
 }
